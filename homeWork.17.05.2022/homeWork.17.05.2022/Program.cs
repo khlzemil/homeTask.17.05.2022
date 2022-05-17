eacf93cc875115ca1d22c43fa269908943a1f395
khlzemil <emilmg@code.edu.az>
2022-05-17 14:59:09 +0400

    Added Hometask

---
+++ homeWork.17.05.2022/homeWork.17.05.2022/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+
+        Console.WriteLine(ReverseString("Emil Yusif Tural Nurlan Faiq"));
+    }
+
+    static string ReverseString(string name)
+    {
+
+        {
+            string reverseName = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverseName += name[i];
+
+            }
+            return reverseName;
+        }
+
+    }
+
+}
